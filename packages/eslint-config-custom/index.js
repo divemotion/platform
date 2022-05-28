57d161a8dc4132d29f0c6773f18c0b9a5ec13a77
Adisak Chaiyakul <adisakchaiyakul@gmail.com>
2022-05-29 03:38:17 +0700

    add eslint import sort

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,8 +1,14 @@
 module.exports = {
   extends: ["next", "prettier"],
+  plugins: ["simple-import-sort", "import"],
   rules: {
     "@next/next/no-html-link-for-pages": "off",
     "react/jsx-key": "off",
+    "simple-import-sort/imports": "error",
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
     semi: ["error", "always"],
     quotes: ["error", "double"],
     indent: [
