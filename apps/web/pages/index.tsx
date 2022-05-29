6fdc294d4025d9830d63d9fe17302edd8208492d
Adisak Chaiyakul <adisakchaiyakul@gmail.com>
2022-05-30 02:15:43 +0700

    test using styled from ui

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,14 @@
-import { Button } from "ui";
+import { Button, styled } from "ui";
+
+const Wrapper = styled.div`
+  background: bisque;
+`;
 
 export default function Web() {
   return (
-    <div>
-      <h1>Web</h1>
+    <Wrapper>
+      <h1 className="text-center font-mono text-3xl font-bold pt-10">Web</h1>
       <Button />
-    </div>
+    </Wrapper>
   );
 }
