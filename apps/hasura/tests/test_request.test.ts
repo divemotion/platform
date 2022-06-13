2fab07bc60d3f44ef0f2966e532569325a5f967b
Adisak Chaiyakul <adisak@onework.dev>
2022-06-13 23:42:36 +0700

    setup testing docker-compose

--- apps/hasura/tests/test_request.test.ts
+++ apps/hasura/tests/test_request.test.ts
@@ -8,7 +8,7 @@ describe("Users", () => {
   describe("query user", () => {
     it("should only see his todos", async () => {
       const userClient = client();
-      const userList = await userClient.UsersWeb();
+      const userList = await userClient.UsersList();
       console.log("userList", userList);
       expect(userList.users.length).toEqual(0);
     });
--- apps/hasura/tests/utils/client.ts
+++ apps/hasura/tests/utils/client.ts
@@ -3,7 +3,7 @@ import { GraphQLClient } from "graphql-request";
 import { getSdk } from "../../graphql/__generated__";
 
 const Client = (): ReturnType<typeof getSdk> => {
-  const client = new GraphQLClient("http://localhost:8080/v1/graphql");
+  const client = new GraphQLClient("http://localhost:5001/v1/graphql");
   return getSdk(client);
 };
 
