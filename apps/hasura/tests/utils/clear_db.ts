417e8e7d8d40aa1f7127d4222fd0d9a9a8ce23bd
Adisak Chaiyakul <adisak@onework.dev>
2022-06-14 18:08:24 +0700

    fix bug test can't clear db

--- apps/hasura/tests/utils/clear_db.ts
+++ apps/hasura/tests/utils/clear_db.ts
@@ -1,13 +1,10 @@
+import dotenv from "dotenv";
 import { Client } from "pg";
 
+dotenv.config();
+
 let connected = false;
-const client = new Client({
-  user: "postgres",
-  host: "localhost",
-  database: "postgres",
-  password: "postgrespassword",
-  port: 5432,
-});
+const client = new Client(process.env.POSTGRES_CONNECTION);
 const getClient = async (): Promise<Client> => {
   if (!connected) {
     await client.connect();
