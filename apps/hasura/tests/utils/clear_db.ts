df146c951d4356d3babb3e974bdc55df2535af84
Rathakit Nopparangsritananon <rathakit@bytemotion.co>
2022-06-29 01:02:45 +0700

    merged

--- apps/hasura/tests/utils/clear_db.ts
+++ apps/hasura/tests/utils/clear_db.ts
@@ -21,7 +21,8 @@ export const closeConnection = async () => {
 
 const clearAllTables = async (): Promise<void> => {
   const client = await getClient();
-  await client.query("TRUNCATE users;");
+  await client.query("TRUNCATE booking CASCADE;");
+  await client.query("TRUNCATE users CASCADE;");
 };
 
 export default clearAllTables;
