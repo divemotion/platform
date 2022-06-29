3adf874b211d2cca10a7bb14da5e8d56b05973c1
Rathakit Nopparangsritananon <rathakit@bytemotion.co>
2022-06-30 00:17:23 +0700

    seed db

---
+++ apps/hasura/seeds/default/1656523000304_tables_seed.sql
@@ -0,0 +1,3 @@
+SET check_function_bodies = false;
+INSERT INTO public.booking_status (id, description_th, description_en, description_zh, created_at, updated_at) VALUES ('WAITING_FOR_CONFIRM', 'รอการยืนยัน', 'Waiting for confirmation', '等待确认中', '2022-06-29 15:52:21.547784+00', '2022-06-29 15:52:21.547784+00');
+INSERT INTO public.booking_status (id, description_th, description_en, description_zh, created_at, updated_at) VALUES ('CONFIRMED', 'ยืนยันการจองเรียบร้อย', 'Booking Confirmed', '确认预订', '2022-06-29 15:53:11.695094+00', '2022-06-29 15:53:11.695094+00');
