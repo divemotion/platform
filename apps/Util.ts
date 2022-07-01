e90bc070daa4eac40fea0c9c8db188e747c658d2
Rathakit Nopparangsritananon <rathakit@bytemotion.co>
2022-07-01 12:35:33 +0700

    Path for boat, trip, cabin

--- apps/Util.ts
+++ apps/Util.ts
@@ -1,1 +1,7 @@
 export const formatOrganizerLogoImagePath = (bucket: string, organizerId: string) => `${bucket}/organizer/${organizerId}/resources/brand_logo`;
+export const formatTripImagePath = (bucket: string, tripId: string) => `${bucket}/trip/${tripId}/resources/images`;
+export const formatTripPromoteImagePath = (bucket: string, tripId: string) => `${bucket}/trip/${tripId}/resources/images/promote`;
+export const formatBoatImagePath = (bucket: string, boatId: string) => `${bucket}/boat/${boatId}/resources/images`;
+export const formatBoatPromoteImagePath = (bucket: string, boatId: string) => `${bucket}/boat/${boatId}/resources/images/promote`;
+export const formatCabinImagePath = (bucket: string, cabinId: string) => `${bucket}/cabin/${cabinId}/resources/images`;
+export const formatCabinPromoteImagePath = (bucket: string, cabinId: string) => `${bucket}/cabin/${cabinId}/resources/images/promote`;
