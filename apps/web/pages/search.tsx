83633d0909ec6ae4bf33e886bdce923cf7d781f0
Adisak Chaiyakul <adisak@onework.dev>
2022-07-08 01:18:21 +0700

    search page add ui

--- apps/web/pages/search.tsx
+++ apps/web/pages/search.tsx
@@ -1,5 +1,6 @@
 import React from "react";
-import { DatePicker, SearchSelect, TripCard } from "ui";
+import { Button, DatePicker, SearchSelect, TripCard } from "ui";
+import { SvgIcon } from "ui/SvgIcon";
 
 import { MainLayout } from "@/components/Layout";
 
@@ -16,23 +17,47 @@ const SearchPage = () => {
   return (
     <MainLayout>
       <div>
-        <div className="p-4">
-          <SearchSelect icon="calendar" noOptionsMessage="ไม่พบการค้นหา" />
-          <br />
-          <DatePicker
-            name="test"
-            placeholder="hi"
-            showMonthYearPicker
-            showFullMonthYearPicker
-            popperPlacement="bottom"
-          />
+        <div id="header" className="relative p-4">
+          <p className="mt-9 mb-7 text-center text-body1 font-semibold text-white">
+            ค้นหาและจองทริปดำน้ำที่ใช่สำหรับคุณ
+          </p>
+          <div className="relative z-10 mx-auto flex max-w-2xl flex-col gap-3 rounded-xl bg-white p-4 shadow-md">
+            <SearchSelect icon="calendar" noOptionsMessage="ไม่พบการค้นหา" />
+            <DatePicker
+              name="test"
+              placeholder="hi"
+              showMonthYearPicker
+              showFullMonthYearPicker
+              popperPlacement="bottom"
+              icon="calendar"
+            />
+            <Button fullWidth color="primary" size="md" border="rounded">
+              <div className="flex items-center justify-center gap-2">
+                <SvgIcon className="h-5 w-5 bg-white" mark icon="search" />
+                ค้นหา
+              </div>
+            </Button>
+          </div>
+          <div className="absolute top-0 left-0 z-0 h-3/4 w-full bg-red-3 bg-[url(https://www.sunrise-divers.com/wp-content/uploads/2017/02/manta-ray-at-koh-bon.jpg)] bg-cover bg-center"></div>
         </div>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {loading && <div>...Loading</div>}
-          {!loading &&
-            data?.trip.map(({ id, name_th, description_th }) => (
-              <TripCard key={id} name={name_th} description={description_th} />
-            ))}
+        <div className="container mx-auto mt-10 flex-1 overflow-hidden px-4">
+          <div id="search-result-header" className="mb-6">
+            <p className="text-body2 font-semibold text-gray-9">
+              ทริปดำน้ำ มิถุนายน 2565
+            </p>
+            <p className="text-body5 text-gray-6">ผลการค้นหา 156 ทริป</p>
+          </div>
+          <div className="mb-4 grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+            {loading && <div>...Loading</div>}
+            {!loading &&
+              data?.trip.map(({ id, name_th, description_th }) => (
+                <TripCard
+                  key={id}
+                  name={name_th}
+                  description={description_th}
+                />
+              ))}
+          </div>
         </div>
       </div>
     </MainLayout>
