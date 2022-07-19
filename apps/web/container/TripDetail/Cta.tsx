4c6b769f0ad15e93c14d8233a0207556d6131b9f
Adisak Chaiyakul <adisak@onework.dev>
2022-07-20 00:41:48 +0700

    add cta button

--- apps/web/container/TripDetail/Cta.tsx
+++ apps/web/container/TripDetail/Cta.tsx
@@ -1,7 +1,29 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
+import tw from "twin.macro";
 import { Button, SvgIcon } from "ui";
 
 export const CTA = () => {
+  const buttonEl = useRef<HTMLDivElement | null>(null);
+  const [isShowCta, setIsShowCta] = useState(false);
+  const handleScroll = () => {
+    const ctaOffset =
+      (buttonEl.current?.offsetTop || 0) +
+      (buttonEl.current?.offsetHeight || 0);
+    if (buttonEl && window.pageYOffset > ctaOffset) {
+      setIsShowCta(true);
+      return;
+    }
+    setIsShowCta(false);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
     <div className="bg-white p-4 shadow-sm md:mb-4 md:rounded-xl">
       <div className="flex-1">
@@ -45,11 +67,20 @@ export const CTA = () => {
             <p className="text-body6 text-emerald-5">฿ 12,000 / คน</p>
           </div>
         </div>
-        <div className="mt-6">
+        <div ref={buttonEl} className="mt-6">
           <Button color="primary" border="rounded" fullWidth size="lg">
             จองเลย
           </Button>
         </div>
+      </div>
+      {/* mobile CTA */}
+      <div
+        css={[isShowCta ? tw`bottom-0` : tw`bottom-[-60px]`]}
+        className="fixed left-0 z-10 block w-full border-t border-gray-3 bg-white px-4 py-3 shadow-md transition-all md:hidden"
+      >
+        <Button color="primary" border="rounded" fullWidth size="sm">
+          จองเลย
+        </Button>
       </div>
     </div>
   );
